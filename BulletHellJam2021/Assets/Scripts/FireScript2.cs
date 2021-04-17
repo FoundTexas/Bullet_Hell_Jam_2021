5c88e3976dc6ecd40f821f05745004f24add6aca
RodoL0204 <rolega01@gmail.com>
2021-04-17 17:32:15 -0500

    spiral bullet hell
    added fire script 2 to shoot in spirals

---
+++ BulletHellJam2021/Assets/Scripts/FireScript2.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript2 : MonoBehaviour
+{
+    [SerializeField]
+    private int rotation = 10;
+
+    [SerializeField]
+    private int bulletsAmount = 3;
+
+    [SerializeField]
+    private float fireRate = 0.1f;
+
+    [SerializeField]
+    private float angle = 0f;
+
+    private Vector2 bulletMoveDirection;
+
+    void Start()
+    {
+        Debug.Log("1");
+        InvokeRepeating("Fire", 0f, fireRate);
+    }
+    public void ResetFRate()
+    {
+        CancelInvoke();
+        InvokeRepeating("Fire", 0f, fireRate);
+    }
+    void Fire()
+    {
+        Debug.Log("2");
+        float angleStep = 360 / bulletsAmount;
+
+        for (int i = 0; i < bulletsAmount; i++)
+        {
+            float bulDirX = transform.position.x + Mathf.Sin((angle + angleStep * i) * Mathf.PI / 180f);
+            float bulDirY = transform.position.y + Mathf.Cos((angle + angleStep * i) * Mathf.PI / 180f);
+
+            Vector3 bulMoveVector = new Vector3(bulDirX, bulDirY, 0f);
+            Vector2 bulDir = (bulMoveVector - transform.position).normalized;
+
+            GameObject bul = FindObjectOfType<BulletPool>().GetBullet();
+            Debug.Log(bul);
+            bul.transform.position = transform.position;
+            bul.transform.rotation = transform.rotation;
+            bul.GetComponent<Bullet>().SetDirection(bulDir);
+            bul.SetActive(true);
+
+            angle += rotation;
+
+            if(angle >= 360)
+            {
+                angle = 0;
+            }
+        }
+    }
+}
