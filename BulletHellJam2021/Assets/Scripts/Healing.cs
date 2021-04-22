29b231b3b661a2c56811e3208eeecb2136c1efbe
GregStillPlays <82214304+GregStillPlays@users.noreply.github.com>
2021-04-22 16:04:07 +0300

    Some cleaning up, making the game less heavy

--- BulletHellJam2021/Assets/Scripts/Healing.cs
+++ BulletHellJam2021/Assets/Scripts/Healing.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class Healing : MonoBehaviour
 {
-    public Animator HealAnim;
-
     void OnTriggerEnter2D(Collider2D player) {
         if(player.gameObject.CompareTag("Player")) {
-            HealAnim.SetTrigger("Pickup");
             Player player1 = player.GetComponent<Player>();
             if(player1.HP < 4) {
                 player1.GetHeal();
